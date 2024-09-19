fe36bb7db712136d740448f565e93713e3a65392
SUSMITA-GOSH <susmitagosh001@gmail.com>
2024-09-20 01:57:17 +0600

    Update StaticClassRe.java

--- StaticClassRe.java
+++ StaticClassRe.java
@@ -1,52 +1,66 @@
-public class StaticClassRe {
-    public static void main(String[] args) {
-        Human kunal = new Human(20, "kunal", 200000, false);
-        System.out.println(kunal.married);
-        Human rahul = new Human(20, "rahul", 200000, false);
-        System.out.println(kunal.population);
-        System.out.println(rahul.population);
-        greeting(); // Call to static method
+class Person {
+    String name;
+    int age;
+    static int totalPeople = 0; 
+
+    // Constructor to initialize a person
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        totalPeople++;  
     }
 
-    static void greeting() {
-        System.out.println("Hello everyone");
-         // Call to static method
+    // Non-static method that can be overridden
+    public void introduce() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
     }
 
-    static void fun() {
-        System.out.println("let's party all night");
-        greeting(); // Call to static method
-    }
-
-    // If we want to access a non-static method in a static class, we have to create a reference of their instance
-    static void unhappy() {
-        // We can't access game() because it's non-static
-        StaticClassRe obj = new StaticClassRe(); // Create an instance of the class
-        obj.games(); // Call the non-static method
-    }
-
-    void games() {
-        System.out.println("we don't like games");
+    // Static method
+    public static void totalPopulation() {
+        System.out.println("Total people created: " + totalPeople);
     }
 }
 
-class Human {
-    int age;
-    int salary;
-    boolean married;
-    String name;
-     long population;
 
-    public Human(int age, String name, int salary, boolean married) {
-        this.age = age;
-        this.name = name;
+class Employee extends Person {
+    int salary;
+
+    
+    public Employee(String name, int age, int salary) {
+        super(name, age);  
         this.salary = salary;
-        this.married = married;
-        this.population += 1;
     }
-    Human()
-    {
-        System.out.println("hi");
+
+    
+    @Override
+    public void introduce() {
+        System.out.println("Hello, my name is " + name + ". I am " + age + " years old and I earn $" + salary + ".");
+    }
+
+    
+    public void work() {
+        System.out.println(name + " is working.");
+    }
+}
+
+public class static2{
+    public static void main(String[] args) {
+       
+        Person john = new Person("John", 30);
+        Employee emma = new Employee("Emma", 25, 50000);
+
+        
+        john.introduce();  
+        emma.introduce();  
+
+        
+        Person.totalPopulation(); 
+
+        
+        emma.work();  
+
+        
+        emma.totalPopulation();
     }
 }
 
